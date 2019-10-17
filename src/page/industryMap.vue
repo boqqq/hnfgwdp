e87ff8a8b6eae7d1b0fe8a7c8deaeae40b61c434
cfchen01 <674441755@qq.com>
2019-10-17 11:00:24 +0800

    添加图表

--- src/page/industryMap.vue
+++ src/page/industryMap.vue
@@ -1,32 +1,117 @@
 <template>
   <div class="industry">
     <div class="chart_main">
-      <el-row>
-        <el-col :span="24">
-          <el-row>
-            <el-col :span="6">
-              <div  class = "fix_left h12">
-              </div>
-            </el-col>
-            <el-col :span="6">
-              <div  class = "fix_center2 h12">
-              </div>
-            </el-col>
-            <el-col :span="6">
-              <div  class = "fix_center2 h12">
-              </div>
-            </el-col>
-            <el-col :span="6">
-              <div  class = "fix_right h12">
-              </div>
-            </el-col>
-          </el-row>
-        </el-col>
-        <el-col :span="24">
-          <div  class = "fix_center2 h12" style="margin: 2vh 2vh">
+      <el-row style=" margin-top: 2vh">
+        <el-col :span="6">
+          <div  class = "fix_left content-padding" style="height: 42vh">
+            <el-row>
+              <el-col :span="17">
+                <span class="title-gb1">十二大重点产业税收前10企业排名</span>
+              </el-col>
+              <el-col :span="7" style="text-align: right">
+                <el-select size="mini" v-model="rankingType" placeholder="请选择">
+                  <el-option
+                    v-for="item in options"
+                    :key="item.value"
+                    :label="item.label"
+                    :value="item.value">
+                  </el-option>
+                </el-select>
+              </el-col>
+            </el-row>
+            <div id="bar_ranking" style="height: 85%; margin-top: 10px">
+
+            </div>
           </div>
         </el-col>
-      </el-row>
+        <el-col :span="6">
+          <div  class = "fix_center content-padding" style="height: 42vh;">
+            <el-row>
+              <el-col>
+                <span class="title-gb1">三次产业增加值</span>
+              </el-col>
+            </el-row>
+            <el-row style="margin: 10px 0">
+              <el-col style="text-align: center">
+                <el-button size="mini" @click="IncrementTypeChange(1)" :class="{'btn-selected':IncrementType == 1}">产业结构</el-button>
+                <el-button size="mini" @click="IncrementTypeChange(2)" :class="{'btn-selected':IncrementType == 2}" style="margin-left: 5px">增加值</el-button>
+                <el-button size="mini" @click="IncrementTypeChange(3)" :class="{'btn-selected':IncrementType == 3}" style="margin-left: 5px">增速</el-button>
+              </el-col>
+            </el-row>
+            <div id="bar_increment" style="height: 80%">
+
+            </div>
+          </div>
+        </el-col>
+        <el-col :span="6">
+          <div  class = "fix_right content-padding" style="height: 42vh">
+            <div id="bar_ranking2" style="height: 100%">
+
+            </div>
+          </div>
+        </el-col>
+        <el-col :span="6">
+          <div  class = "fix_right content-padding" style="height: 42vh">
+            <el-row>
+              <el-col>
+                <span class="title-gb1">三次产业增加值</span>
+              </el-col>
+            </el-row>
+            <div id="bar_newly" style="height: 85%; margin-top: 10px">
+
+            </div>
+          </div>
+            </el-col>
+            <el-col :span="24">
+              <div  class = "fix_left fix_right" style="height: 45.2vh; margin-top: 2vh">
+                <el-row>
+                  <el-col style="width: 23%">
+                    <div  style="height: 45.2vh" class="content-padding">
+                      <el-row>
+                        <el-col>
+                          <span class="title-gb1">十二大产业税收贡献前5名</span>
+                        </el-col>
+                      </el-row>
+                      <div id="bar_contribution" style="height: 85%; margin-top: 10px">
+
+                      </div>
+                    </div>
+                  </el-col>
+                  <el-col :span="2" style="height: 45.2vh; width: 8.5%">
+                    <div class="rate-box" style=" margin-top: 3vh">
+                      <div class="rate-box-in">
+                        <el-row style="padding: 5px 0">
+                          <span class="rate-box-text">十二大重点产业<br>增加值占GDP比重</span>
+                        </el-row>
+                        <el-row style="padding: 5px; text-align: right">
+                          <span class="rate-box-number">83.21%</span>
+                        </el-row>
+                      </div>
+                    </div>
+                    <div class="rate-box" style="margin: 1.5vh 0">
+
+                    </div>
+                    <div class="rate-box" style="margin-bottom: 3vh">
+
+                    </div>
+                  </el-col>
+                  <el-col style="height: 45.2vh; width: 23%">
+                    <div class="content-padding" style="height: 100%;">
+                      <el-row>
+                        <el-col :span="17">
+                          <span class="title-gb1">十二大重点产业投资额及增速</span>
+                        </el-col>
+                      </el-row>
+                      <div id="bar_investment" style="height: 85%; margin-top: 10px">
+
+                      </div>
+                    </div>
+                  </el-col>
+                  <el-col :span="9">123</el-col>
+                </el-row>
+                </div>
+            </el-col>
+          </el-row>
     </div>
   </div>
 </template>
@@ -102,6 +187,7 @@
       this.initIncrement()
       // this.initNewly()
       this.initContribution()
+      this.initInvestment()
     },
     methods: {
       IncrementTypeChange(val){
@@ -256,19 +342,34 @@
               type:'bar',
               barWidth : 15,
               stack: '产业结构',
-              data:[220, 432, 501, 534, 790, 830, 920, 1021]
+              data:[220, 432, 501, 534, 790, 830, 920, 1021],
+              itemStyle: {
+                normal: {
+                  color: '#DF2DFF',
+                }
+              }
             },
             {
               name:'第二产业',
               type:'bar',
               stack: '产业结构',
-              data:[120, 132, 101, 134, 290, 230, 220, 330]
+              data:[120, 132, 101, 134, 290, 230, 220, 330],
+              itemStyle: {
+                normal: {
+                  color: '#139DFF',
+                }
+              }
             },
             {
               name:'第三产业',
               type:'bar',
               stack: '产业结构',
-              data:[60, 72, 71, 74, 190, 130, 110,230]
+              data:[60, 72, 71, 74, 190, 130, 110,230],
+              itemStyle: {
+                normal: {
+                  color: '#E11054',
+                }
+              }
             },
           ]
         };
@@ -442,7 +543,7 @@
             type: 'pie',
             radius: ['30%', '40%'],
             center: ['50%', '50%'],
-            color: ['#80C269', '#00FFFF', '#0090F1', '#FFA800','#4658F6'],
+            color: ['#3F61FF', '#C94B89', '#9072EE', '#54BB4D','#929090'],
             itemStyle:{
               //  normal: {
               //     borderWidth: 5,
@@ -487,7 +588,7 @@
                 formatter: '{b|{b}}',
                 rich: {
                   b: {
-                    fontSize: 10,
+                    fontSize: 12,
                     color: '#FFF',
                     align: 'left',
                     padding: 1
@@ -513,11 +614,155 @@
         };
         bar_contribution.setOption(option)
       },
+      initInvestment(){
+        console.log('=====')
+        var bar_investment = echarts.init(document.getElementById('bar_investment'));
+
+        // Generate data
+        var category = ['2017Q3','2017Q4','2018Q1','2018Q2','2018Q3','2018Q4',];
+        var dottedBase = [];
+        var lineData = [123,211,99
+          ,432,321,544];
+        var barData = [55,67,43
+          ,87,65,89];
+        var rateData = [];
+        32808
+        for (var i = 0; i < 33; i++) {
+          // var date = i+2001;
+          // category.push(date)
+          var rate=barData[i]/lineData[i]*100;
+          rateData[i] = rate.toFixed(2);
+        }
+
+
+        var option = {
+          title: {
+            text: '',
+            x: 'center',
+            y: 0,
+            textStyle:{
+              color:'#B4B4B4',
+              fontSize:16,
+              fontWeight:'normal',
+            },
+
+          },
+          // backgroundColor: '#0d235e',
+          tooltip: {
+            trigger: 'axis',
+            backgroundColor:'rgba(255,255,255,0.1)',
+            axisPointer: {
+              type: 'shadow',
+              label: {
+                show: true,
+                backgroundColor: '#7B7DDC'
+              }
+            }
+          },
+          legend: {
+            data: ['投资额（亿元）', '增速（%）',],
+            textStyle: {
+              color: '#B4B4B4'
+            },
+            // top:'7%',
+          },
+          grid: {
+            left: '10',
+            right: '6',
+            bottom: '0',
+            top:'50',
+            containLabel: true
+          },
+          xAxis: {
+            data: category,
+            axisLine: {
+              lineStyle: {
+                color: '#B4B4B4'
+              }
+            },
+            axisTick:{
+              show:false,
+            },
+          },
+          yAxis: [{
+
+            // splitLine: {show: false},
+            axisTick: {
+              show: false
+            },
+            axisLine: {
+              show: false,
+              lineStyle: {
+                color: '#B4B4B4',
+              }
+            },
+
+            axisLabel:{
+              formatter:'{value} ',
+            }
+          },
+            {
+
+              splitLine: {show: false},
+              axisTick: {
+                show: false
+              },
+              axisLine: {
+                show:false,
+                lineStyle: {
+                  color: '#B4B4B4',
+                }
+              },
+              axisLabel:{
+                show:false,
+                formatter:'{value} ',
+              }
+            }],
+
+          series: [{
+            name: '增速（%）',
+            type: 'line',
+            // smooth: true,
+            showAllSymbol: true,
+            symbol: 'emptyCircle',
+            symbolSize: 8,
+            yAxisIndex: 1,
+            itemStyle: {
+              color:'#fff'
+            },
+            lineStyle: {
+              color: "#3EACE5"
+            },
+            data: rateData
+          },
+
+            {
+              name: '投资额（亿元）',
+              type: 'bar',
+              barWidth: 15,
+              itemStyle: {
+                normal: {
+                  // barBorderRadius: 5,
+                  color: new echarts.graphic.LinearGradient(
+                    0, 0, 0, 1,
+                    [
+                      {offset: 0, color: '#956FD4'},
+                      {offset: 1, color: '#3EACE5'}
+                    ]
+                  )
+                }
+              },
+              data: barData
+            }
+          ]
+        };
+        bar_investment.setOption(option)
+      },
     }
   }
 </script>
 <style lang="scss">
-  $title-color1:#145FA1;
+  $title-color1:#0090E0;
   .industry{
     .title-gb1{
       color: $title-color1;
@@ -526,6 +771,16 @@
     }
     .content-padding{
       padding: 5px 10px;
+    }
+    .fix_left{
+      margin-top: 2vh;
+    }
+    .fix_right{
+      margin-top: 2vh;
+    }
+    .fix_center{
+      border: 1px solid #23A9F3;
+      margin: 2vh 2vh 0;
     }
     .title-gb1::before{
       width: 10px;
@@ -605,6 +860,27 @@
     .btn-selected{
       background-color: $title-color1;
       color: #fff;
+    }
+    .rate-box-in{
+      background-color: #1D3C7D;
+      height: 100%;
+      opacity: 0.8;
+    }
+    .rate-box{
+      height: 11.5vh;
+      border: 1px solid #999;
+      background-color: transparent;
+      padding: 2px;
+    }
+    .rate-box-text{
+      color: #FFF;
+      font-size: 12px;
+      white-space: nowrap
+    }
+    .rate-box-number{
+      color: #FFF;
+      font-size: 16px;
+      text-shadow: 0 0 10px #04cbe0;
     }
   }
 </style>
